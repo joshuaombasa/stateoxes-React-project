e397544f535c55daa112ea4699c7c44e8f73ca57
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-29 20:21:33 +0300

    added data

---
+++ public/boxes.js
@@ -0,0 +1,31 @@
+export const boxes = [
+    {
+        id: 1,
+        on: false
+    },
+    {
+        id: 2,
+        on: false
+    },
+    {
+        id: 3,
+        on: false
+    },
+    {
+        id: 4,
+        on: false
+    },
+    {
+        id: 5,
+        on: false
+    },
+    {
+        id: 6,
+        on: false
+    }
+    ,
+    {
+        id: 7,
+        on: false
+    }
+]
