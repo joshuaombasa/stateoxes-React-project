0f82a2f0ee599b746d056d351e721866a8d4582e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-12 23:43:02 +0300

    updated app logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,22 @@ function App() {
 
   const [boxesData, setBoxesData] = useState(null)
 
+
+  function toggle(id) {
+    setBoxesData(prevBoxesData => {
+      return prevBoxesData.map(item => {
+        return item.id === id ? {...item, on: !item.on} : item
+      } )
+    })
+  }
+
   useEffect(() => {
     setBoxesData(boxes)
   },[])
 
   let boxesJsx 
   if (boxesData) {
-    boxesJsx = boxesData.map(box => <Box key={box.id} box={box}/>)
+    boxesJsx = boxesData.map(box => <Box key={box.id} box={box} toggle={toggle}/>)
   }
 
   return (
